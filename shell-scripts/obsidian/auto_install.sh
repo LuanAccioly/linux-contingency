275de5dca9b95ff132562d80edf0ee77ce13aee4
Luan Accioly <acciolyluan@gmail.com>
2023-10-27 23:16:14 -0300

    obsidian auto_install script

---
+++ shell-scripts/obsidian/auto_install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+purple='\033[0;35m'
+green='\033[0;32m'
+
+# Reseta a cor para a cor padrão no final
+clear='\033[0m'
+
+# Imprime o banner com a cor roxa
+echo -e "${purple}"
+echo "  _____ _         _     _ _             "
+echo " |  _  | |       (_)   | (_)            "
+echo " | | | | |__  ___ _  __| |_  __ _ _ __  "
+echo " | | | | '_ \/ __| |/ _  | |/ _  | '_ \ "
+echo " \ \_/ / |_) \__ \ | (_| | | (_| | | | |"
+echo "  \___/|_.__/|___/_|\__,_|_|\__,_|_| |_|"
+echo "                                         "
+echo -e "${reset}"
+
+
+# Função para exibir uma mensagem de sucesso
+success_message() {
+  echo -e "${green}✓ $1${clear}"
+}
+
+# Clone o repo obsidian-scripts sem exibir mensagens
+git clone https://github.com/seu-usuario/obsidian-scripts.git > /dev/null 2>&1
+success_message "Clonagem do repositório obsidian-scripts"
+
+# Clone o repo linux-scripts sem exibir mensagens
+git clone https://github.com/seu-usuario/linux-scripts.git > /dev/null 2>&1
+success_message "Clonagem do repositório linux-scripts"
+
+# Instale o Obsidian usando o Yay
+yay -S obsidian
+
+# Verifique se a instalação do Obsidian foi bem-sucedida
+if [ $? -eq 0 ]; then
+  success_message "Instalação do Obsidian"
+else
+  echo "Erro na instalação do Obsidian."
+fi
+
+# Adicione o alias ao arquivo ~/.zshrc
+echo 'alias obs="~/linux-scripts/obsidian/pc/pc.sh"' >> ~/.zshrc
+
+# Atualize o arquivo ~/.zshrc para refletir as alterações
+source ~/.zshrc
+
+success_message "Alias adicionado ao arquivo ~/.zshrc"
+
+echo "Configuração concluída. Você pode executar o Obsidian usando o comando 'obs'."
